052f2da928ac2ffc8492cd5be2f4c3fa8bde8fbf
Vital D <netmin@pm.me>
2023-09-20 21:07:01 +0500

    fix search-input

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,21 +1,27 @@
 "use client";
+
 import qs from "query-string";
-import {Search} from "lucide-react";
-import {Input} from "@/components/ui/input";
-import {useRouter, useSearchParams} from "next/navigation";
 import {ChangeEventHandler, useEffect, useState} from "react";
+import {useRouter, useSearchParams} from "next/navigation";
+import {Search} from "lucide-react";
+
+import {Input} from "@/components/ui/input";
 import {useDebounce} from "@/hooks/use-debounce";
 
 export const SearchInput = () => {
     const router = useRouter();
-    const searchParam = useSearchParams();
-    const categoryId = searchParam.get("categoryId");
-    const name = searchParam.get("name");
+    const searchParams = useSearchParams();
+
+    const categoryId = searchParams.get("categoryId");
+    const name = searchParams.get("name");
+
     const [value, setValue] = useState(name || "");
     const debouncedValue = useDebounce<string>(value, 500);
-    const onChange = ChangeEventHandler<HTMLInputElement> = (e) => {
+
+
+    const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
         setValue(e.target.value);
-    }
+    };
 
     useEffect(() => {
         const query = {
@@ -25,22 +31,21 @@ export const SearchInput = () => {
 
         const url = qs.stringifyUrl({
             url: window.location.href,
-            query,
-        }, {skipEmptyString: true, skipNull: true});
+            query
+        }, {skipNull: true, skipEmptyString: true});
 
         router.push(url);
-    }, [debouncedValue, router, categoryId]);
-
+    }, [debouncedValue, router, categoryId])
 
     return (
         <div className="relative">
-            <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
+            <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground"/>
             <Input
                 onChange={onChange}
                 value={value}
                 placeholder="Search..."
                 className="pl-10 bg-primary/10"
-                />
+            />
         </div>
     )
-}
+};
